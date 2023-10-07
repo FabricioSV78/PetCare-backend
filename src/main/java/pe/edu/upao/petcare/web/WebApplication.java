32acfba86563a867967f79b2f842df39865c683f
Fabricio <fabricioseminarioo2004@gmail.com>
2023-10-07 22:53:26 +1300

    first commit

---
+++ src/main/java/pe/edu/upao/petcare/web/WebApplication.java
@@ -0,0 +1,13 @@
+package pe.edu.upao.petcare.web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebApplication.class, args);
+	}
+
+}
